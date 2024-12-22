7c02d065754d9fc053a222fa7c49f7cf4e25d39c
Mateus Schuch da Silva <mateus-silva@procergs.rs.gov.br>
2024-12-22 15:35:12 -0300

    Criando listas

---
+++ JS_Explorando_a_linguagem/listas.js
@@ -0,0 +1,7 @@
+console.log(`Trabalhando com listas`);
+const salvador = `Salvador`;
+const saoPaulo = `São Paulo`;
+const rioDeJaneiro = `Rio de Janeiro`;
+
+console.log("Destinos possíveis");
+console.log(salvador, saoPaulo, rioDeJaneiro);
